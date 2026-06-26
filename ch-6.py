2ca3a80a266b3cba38bc6b9c2bd0fdb98363937c
Manish <namish9305@gmail.com>
2026-06-26 07:23:37 +0530

    day6 function and recursion

--- ch-6.py
+++ ch-6.py
@@ -1,4 +1,36 @@
-a =20
-b=20
-sum =a+b
-print(sum)
+#function 
+
+"""
+def calulategmean(a,b):
+    mean= (a*b)/(a+b)
+    print (mean)
+
+
+def isgreater(a,b):#this is code for if else statement in porgrome we donot write 
+                   #many time in this code. we write only one time in the code and define this as a fnction use so many time.
+    if(a>b):
+        print("first number is greater")
+    elif(a==b):
+        print("both number is equal ")
+    else:
+        print("second number is greaater")
+def islower(a,b):
+    pass# pass mean do noting and go to next part.
+
+
+a=int(input("enter a number value"))
+b=int(input("enter b number value"))
+isgreater(a,b)
+calulategmean(a,b)
+
+
+c=int(input("enter c number value"))
+d=int(input("enter d number value"))
+isgreater(c,d) 
+calulategmean(c,d)
+
+
+f= int(input("enter f number value"))
+e=int(input("enter eororngg E is number value"))
+calulategmean(f,e)
+"""
